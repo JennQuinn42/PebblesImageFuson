b62955e83e8b34840b017e9023e03c1470d6b694
ThomasMcSherry <thomasmcsherry92@gmail.com>
2015-09-30 10:27:22 +0100

    Create ColourPanel.java

---
+++ ColourPanel.java
@@ -0,0 +1,12 @@
+package com.jt.beads;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class ColourPanel extends JPanel{
+
+	public ColourPanel(){
+		
+		JScrollPane scroller = new JScrollPane(this);
+	}
+}
