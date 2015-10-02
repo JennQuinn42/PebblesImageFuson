39f59b4e5dc8ff4a170a13200db70ac17422e91a
JennQuinn42 <jennquinn42@gmail.com>
2015-10-02 11:38:09 +0100

    Update NewDialog.java
    Dialog is now same colour as parent

--- NewDialog.java
+++ NewDialog.java
@@ -24,46 +24,63 @@ public class NewDialog extends JDialog {
 	private BufferedImage imageFile;
 	private Editor frame;
 
-	public static void main(String[] args){
-		Editor e = new Editor();
-		e.setVisible(false);
-		new OpenDialog(e).setVisible(true);
-	}
+	
 	
 	/**
 	 * Create the dialog.
 	 */
 	public NewDialog(final Editor frame) {
 		setTitle("New Image");
-		setBounds(100, 100, 167, 228);
+		setBounds(100, 100, 178, 228);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
+		Color backgroundColour = frame.getContentPane().getBackground();
+		Color textColour = frame.getContentPane().getForeground();
+		
+		contentPanel.setBackground(backgroundColour);
+		contentPanel.setForeground(textColour);
+		setBackground(backgroundColour);
+		setForeground(textColour);
+		
 		JLabel lblPixelsWidth = new JLabel("Pixels in Width");
 		lblPixelsWidth.setBounds(10, 11, 91, 20);
+		lblPixelsWidth.setBackground(backgroundColour);
+		lblPixelsWidth.setForeground(textColour);
+		
 		contentPanel.add(lblPixelsWidth);
 		
 		JLabel lblPixelsHeight = new JLabel("Pixels in Height");
 		lblPixelsHeight.setBounds(10, 73, 91, 21);
+		lblPixelsHeight.setBackground(backgroundColour);
+		lblPixelsHeight.setForeground(textColour);
 		contentPanel.add(lblPixelsHeight);
 		
-		JSpinner spnrWidth = new JSpinner();
+		final JSpinner spnrWidth = new JSpinner();
 		spnrWidth.setBounds(10, 42, 68, 20);
+		spnrWidth.setBackground(backgroundColour);
+		spnrWidth.setForeground(textColour);
 		spnrWidth.setModel(new SpinnerNumberModel(new Integer(100), new Integer(2), null, new Integer(1)));
 		contentPanel.add(spnrWidth);
 		
-		JSpinner spnrHeight = new JSpinner();
+		final JSpinner spnrHeight = new JSpinner();
 		spnrHeight.setBounds(10, 105, 68, 20);
+		spnrHeight.setBackground(backgroundColour);
+		spnrHeight.setForeground(textColour);
 		spnrHeight.setModel(new SpinnerNumberModel(new Integer(100), new Integer(2), null, new Integer(1)));
 		contentPanel.add(spnrHeight);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			buttonPane.setBackground(backgroundColour);
+			buttonPane.setForeground(textColour);
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton btnCreate = new JButton("Create");
+				btnCreate.setBackground(backgroundColour);
+				btnCreate.setForeground(textColour);
 				btnCreate.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent arg0) {
 						int dummySize = 3;
@@ -86,6 +103,8 @@ public class NewDialog extends JDialog {
 		
 			{
 				JButton btnCancel = new JButton("Cancel");
+				btnCancel.setBackground(backgroundColour);
+				btnCancel.setForeground(textColour);
 				btnCancel.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						((JDialog)contentPanel.getParent().getParent().getParent().getParent()).dispose();						
@@ -95,5 +114,5 @@ public class NewDialog extends JDialog {
 				buttonPane.add(btnCancel);
 			}
 		}
-	}
+	}	
 }
